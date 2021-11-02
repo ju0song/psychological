b5f2ed9e6d79a5c0af9c29988ecad306cde74178
ju0song <74412161+ju0song@users.noreply.github.com>
2021-11-03 00:37:28 +0900

    Add files via upload

--- sympathytest/app/src/main/java/com/example/sympathytest/fragment/Mainfragment.kt
+++ sympathytest/app/src/main/java/com/example/sympathytest/fragment/Mainfragment.kt
@@ -35,7 +35,7 @@ class Mainfragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mainfragment, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     companion object {
